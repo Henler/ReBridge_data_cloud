c8a8d33d7ea24050c3136702238198762779b5a0
Axel Theorell <a.theorell@fz-juelich.de>
2020-03-03 17:08:57 +0100

    NPM downloads fontawesome fix

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './static/css/sb-admin-2.min.css';
-import './static/vendor/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Routes from './routes/Routes';
